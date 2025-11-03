68ecc1de5076885d67342f1ab4692dece03eac11
S-M-Mazharul-Islam-RIfat <smmjrifat@gmail.com>
2025-11-03 17:00:58 +0600

    added initial folder structure and setup db connection

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { Server } from 'http';
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+let server: Server;
+
+async function main() {
+   try {
+      await mongoose.connect(config.database_url as string);
+      server = app.listen(config.port, () => {
+         console.log(`mind mesh ai is listening on port ${config.port}`);
+      });
+   } catch (err) {
+      console.log(err);
+   }
+}
+
+main();
